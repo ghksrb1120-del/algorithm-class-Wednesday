1cdd618178e06d0a6beffaf5d590ceb5ca74255e
ghksrb1120-del <ghksrb1120@gmail.com>
2025-10-15 17:16:47 +0900

    tree

--- tree/expressionTree_Wed.py
+++ tree/expressionTree_Wed.py
@@ -1,5 +1,5 @@
 # ExpressionTree.py
-from binary_tree_Tues import BTNode, inorder, postorder, preorder
+from binary_tree_Wed import BTNode, inorder, postorder, preorder
 
 # 1. 모스코드 결정트리
 # ===== 모스 코드 테이블 (A~Z) =====
@@ -27,61 +27,67 @@ MORSE_TABLE = [
 #===================================================
 # 테스트 프로그램 : 결정 트리(이진 트리) 기반 모스 코드 트리
 #===================================================   
+if __name__ == "__main__":
+    morseCodeTree = make_morse_tree()
+    s = input("입력 문장 : ").strip() # GAMEOVER, DATA
 
-# if __name__ == "__main__":
-#     morseCodeTree = make_morse_tree()
-#     s = input("입력 문장 : ").strip() # GAMEOVER, DATA
+    print("Encoding: ", s)
+    mlist = [ ]
+    for ch in s:
+      code = encode(ch) 
+      mlist.append(code)
+    print("Morse Code:", mlist)
 
-#     mlist = [ ]
-#     for ch in s:
-#       code = encode(ch) 
-#       mlist.append(code)
-#     print("Morse Code:", mlist)
-
-#     print("Decoding: ", end='')
-#     for code in mlist:
-#         print(decode(morseCodeTree, code), end='')
-#     print()
+    print("Decoding: ", end='')
+    for code in mlist:
+        print(decode(morseCodeTree, code), end='')
+    print()
    
 #==================================================
 # 테스트 프로그램 코드 4.17: 수식 트리 생성 및 순회 및 평가
 #=====================================================  
-# if __name__ == "__main__":
-#     # (1+3) * (4/2)
-#     # expr = ['1', '3', '+', '4', '2', '/', '*']
-#     str = input("입력(후위표기): ")
-#     expr = str.split()
-#     print("토큰분리(expr): ", expr)
+print("\n=== 수식 트리 생성 및 평가 테스트 ===")  
+if __name__ == "__main__":
+    # (1+3) * (4/2)
+    # expr = ['1', '3', '+', '4', '2', '/', '*']
+    str = input("입력(후위표기): ") # 공백을 사용하여 입력-> 1 3 + 4 2 / * 
+    expr = str.split() 
+    print("토큰분리(expr): ", expr)
 
-#     root = buildTree(expr)
-#     print("트리 루트:", root)
+    root = buildTree(expr) 
+    print("트리 루트:", root)
 
-#     preorder(root)
-#     print()
-#     inorder(root)
-#     print()
-#     postorder(root)
-#     print()
-#     print("수식 트리 값 평가:", evaluate(root))             
+    # 트리 순회 이용하여 수식 표기 
+    print("전위표기식=>", end=' ')
+    preorder(root) 
+    print()
+    
+    print("중위표기식=>", end=' ')
+    inorder(root) 
+    print()
 
+    print("후위표기식=>", end=' ')
+    postorder(root) 
+    print()
+    
+    # 수식 평가
+    print("수식 트리 값 평가:", evaluate(root))        
 #=============================
 # 테스트 프로그램 QUIZ p.150 
 #=============================
-# if __name__ == "__main__":
-#     # Construct the expression tree for expr = 2 1 3 + * 8 4 / -
-#     # Tree structure:
-#     # 트리 구조:
-#     #        -
-#     #      /   \
-#     #     *      /
-#     #    / \   /  \
-#     #   2   +  8   4
-#     #      / \
-#     #     1  3
-#     # 중위식: (2 * (1 + 3)) - (8 / 4)
-#     postfix_expr = ['2', '1', '3', '+', '*', '8', '4', '/', '-']
-#     root = buildTree(postfix_expr)  # 수식 트리 생성
-#     print(root)
-#     result = evaluate(root)         # 평가
-#     print("평가 결과 =", result)    # 예상 결과: 6.0
-
+if __name__ == "__main__":
+    # 후위표기식 :  2 1 3 + * 8 4 / -
+    # 트리 구조:
+    #        -
+    #      /   \
+    #     *      /
+    #    / \   /  \
+    #   2   +  8   4
+    #      / \
+    #     1  3
+    # 중위표기식: (2 * (1 + 3)) - (8 / 4)
+    postfix_expr = ['2', '1', '3', '+', '*', '8', '4', '/', '-']
+    root = buildTree(postfix_expr)  # 수식 트리 생성
+    print(root)
+    result = evaluate(root)         # 평가
+    print("평가 결과 =", result)    # 예상 결과: 6.0
